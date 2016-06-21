636348b6a257d489796b72a1c2a68b06aeb78cfc
Jung Se Yeon <devhackin@gmail.com>
2016-06-21 09:00:28 +0900

    chore(init): 처음으로 커밋하다

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+/*
+ * Application
+ */
+
+$(document).tooltip({
+    selector: "[data-toggle=tooltip]"
+})
+
+$( '#top' ).click( function() {
+    $( 'html, body' ).animate( { scrollTop : 0 }, 400 );
+    return false;
+} );
+
+$(window).scroll(function(){
+    if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        if ($(window).scrollTop() >= 550)
+        {
+            $(".customtel-wrapper").css({position:'fixed', top:'0'});
+        }
+        else
+        {
+            $(".customtel-wrapper").css({position:'relative', top: '0px'});
+        }
+    }
+});
